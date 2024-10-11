8c0c3abd5d4d20951b9d8bed9392f8d6aac5a298
Gülcan Kamat <gulcankamat@gmail.com>
2024-10-11 11:03:00 +0200

    Modifying k6 file.

--- tests/feedback-api.load.js
+++ tests/feedback-api.load.js
@@ -6,14 +6,14 @@ const BASE_URL = __ENV.BASE_URL || 'http://localhost:3000';
 
 export let options = {
     stages: [
-        { duration: '30s', target: 100 },
-        { duration: '1m', target: 250 },
-        { duration: '1m', target: 500 },
-        { duration: '2m', target: 0 }
+        { duration: '30s', target: 100 },  // Ramp-up to 100 users in 30 seconds
+        { duration: '1m', target: 250 },   // Ramp-up to 250 users in 1 minute
+        { duration: '1m', target: 500 },   // Ramp-up to 500 users in 1 minute
+        { duration: '2m', target: 0 }      // Ramp-down to 0 users in 2 minutes
     ]
 };
 
 export default function () {
-    http.get('${BASE_URL}/feedback');
+    http.get(`${BASE_URL}/feedback`);  // Use backticks for string interpolation
     sleep(1);
 }

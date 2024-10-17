80371154914a2a55659c3157da86238479a50d52
Gülcan Kamat <gulcankamat@gmail.com>
2024-10-17 14:13:46 +0200

    Fixing.

--- terraform_HA_14.10/hello_world.js
+++
@@ -1,8 +0,0 @@
-// hello_world.js
-export async function handler(event) {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello, world!'),
-    };
-    return response;
-}
